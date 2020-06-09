89d31434e62f6670d62f11e30a3735ba21378728
Yurii Khomenko <yuriikhomenko@macBookProYurii.home>
2020-06-09 20:00:22 +0200

    Добавляет React и создает первые компоненты

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable indent */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  PLACES_COUNT: 10
+};
+
+
+ReactDOM.render(
+  <App
+    placesCount={Settings.PLACES_COUNT} />,
+  document.querySelector(`#root`)
+);
